563c4822fe6ffba8cf45844f4457d48be3ad9928
Andrew Briz <briz.andrew@gmail.com>
2022-04-01 11:49:17 -0400

    Some more tweaks

--- src/utils/toNotionTime.js
+++ src/utils/toNotionTime.js
@@ -1,7 +1,7 @@
 export default function toNotionTime(dateObj) {
   const systemOffset = `${(dateObj.getTimezoneOffset()) / 60}`;
   const offsetZeroPadded = systemOffset.length === 1 ? `0${systemOffset}` : systemOffset;
-  const offsetWithSign = parseInt(systemOffset, 10) > 0 ? `-${offsetZeroPadded}` : offsetZeroPadded;
+  const offsetWithSign = parseInt(systemOffset, 10) > 0 ? `-${offsetZeroPadded}` : `+${offsetZeroPadded}`;
 
   const year = dateObj.getFullYear();
   const month = `${dateObj.getMonth() + 1}`;
